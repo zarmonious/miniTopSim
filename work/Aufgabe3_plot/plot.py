9ad5a814232607fb54fd01eb1df07b507468e932
lexihaberl <74511314+lexihaberl@users.noreply.github.com>
2020-11-21 15:41:17 +0100

    Aufgabe3_plot: Rudimentary Implementation of interactive Plotting

--- work/Aufgabe3_plot/plot.py
+++ work/Aufgabe3_plot/plot.py
@@ -12,27 +12,28 @@ def read_srf_file(filename):
         return None
     
     if (filename.endswith(('.srf_save', '.srf')) == False):
-        print('Error: WrongFileEnding')
+        print('Error: WrongFileExtension')
         return None
     
     time = list()
     npoints = list()
-    xpoints = list()
-    ypoints = list()
-    xpoint = None;
-    ypoint = None;
+    xPointsList = list()
+    yPointsList = list()
+    xpoints = None;
+    ypoints = None;
     i=0
     j=0
 
+    #Format abfrage noch inkludieren (erste zeile) und ob j==npoints
     for line in file:
         if 'surface:' in line:
             stringArray = line.split(' ')
             time.append(float(stringArray[1]))
             npoints.append(int(stringArray[2]))
-            xpoint = np.empty(npoints[i], dtype=np.float64)
-            ypoint = np.empty(npoints[i], dtype=np.float64)
-            xpoints.append(xpoint)
-            ypoints.append(ypoint)
+            xpoints = np.empty(npoints[i], dtype=np.float64)
+            ypoints = np.empty(npoints[i], dtype=np.float64)
+            xPointsList.append(xpoints)
+            yPointsList.append(ypoints)
             #print(xpoints)
 
             #print(stringArray)
@@ -44,19 +45,30 @@ def read_srf_file(filename):
             j=0
         else:
             stringArray = line.split(' ')
-            xpoint[j] = float(stringArray[0])
-            ypoint[j] = float(stringArray[1])
+            xpoints[j] = float(stringArray[0])
+            ypoints[j] = float(stringArray[1])
             j=j+1
-
-    return xpoints, ypoints
-
     
+    file.close()
 
+    return xPointsList, yPointsList, time
+
+
+def on_key_press(event):
+    print('you pressed', event.key)
+    return None
+
+def plot_interactive(xval, yval, time):
+    fig = plt.figure()
+    fig.canvas.mpl_connect('key_press_event', on_key_press)
+    plt.title('Time = ' + str(time[0]) + 's')
+    plt.plot(xval[0],yval[0])
+    plt.show()
     
-def plot_srf(xval, yval):
-    
+def plot_all_srf(xval, yval,time ):
     for x in range(len(xval)):
         plt.figure(x)
+        plt.title('Time = ' + str(time[x]) + 's')
         plt.plot(xval[x],yval[x])
     return None
 
@@ -64,7 +76,8 @@ def plot_srf(xval, yval):
 
 
 if __name__ == '__main__':
-    xval, yval = read_srf_file('trench.srf_save')
-    plot_srf(xval, yval)
+    xval, yval, time = read_srf_file('trench.srf_save')
+    #plot_all_srf(xval, yval, time)
+    plot_interactive(xval,yval, time)
 
-# %%
+#%%
