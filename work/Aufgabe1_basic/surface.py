aba91ebee403e8f1f696f64512e7028af6a5fa95
haberger <d.haberger@hotmail.com>
2020-11-23 20:51:52 +0100

    Aufgabe1_basic: fix norm vec, plot plus write func

--- work/Aufgabe1_basic/surface.py
+++ work/Aufgabe1_basic/surface.py
@@ -3,48 +3,39 @@ import init_surface
 import matplotlib.pyplot as plt
 
 
-
 class Surface:
 
     def __init__(self):
-        self.dx = 1
-        self.xVals = np.arange(-50,51,self.dx)
-        self.yVals = init_surface.init_surface(self.xVals)      
+        self.xVals = np.arange(-50., 51., 1.)
+        self.yVals = init_surface.init_surface(self.xVals)
 
     def normal_vector(self):
-        #x = np.zeros_like(self.xVals)
-        x = np.full_like(self.xVals, 2*self.dx)
+        x = np.zeros_like(self.xVals)
         y = np.zeros_like(self.yVals)
 
-        #x[0] = self.xVals[1] - self.xVals[0]
-        x[0] = self.dx
+        x[0] = self.xVals[1] - self.xVals[0]
         y[0] = self.yVals[1] - self.yVals[0]
-        
-        #x[-1] = self.xVals[-1] - self.xVals[-2]
-        x[-1] = self.dx
+
+        x[-1] = self.xVals[-1] - self.xVals[-2]
         y[-1] = self.yVals[-1] - self.yVals[-2]
-        
-        
-        y[1:-1] = np.array(self.yVals[2::] -self.yVals[:-2:])
-        #x[1:-1] = np.array(self.xVals[2::] - self.xVals[:-2:])
-    
+
+        y[1:-1] = np.array(self.yVals[2::] - self.yVals[:-2:])
+        x[1:-1] = np.array(self.xVals[2::] - self.xVals[:-2:])
+
         magnitude = np.linalg.norm([x, y], axis=0)
-        
-        return np.array((y / magnitude, -x / magnitude))
 
+        return (y / magnitude), (-x / magnitude)
 
-    def plot(self):
+    def plot(self, time):
 
-        plt.plot(self.xVals,self.yVals)
+        plt.plot(self.xVals, self.yVals, "x-", label=f"t = {time}")
         plt.title('Surface')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.show()
+        plt.xlabel('x[nm]')
+        plt.ylabel('y[nm]')
 
-    def write(self):
-        return 0
-    
-
-s = Surface()
-x, y = s.normal_vector()
-s.plot()
+    def write(self, time, filename):
+        with open(filename, 'a') as f:
+            f.write(
+                f"surface: {time} {len(self.xVals)} x-positions y-positions\n")
+            for x, y in zip(self.xVals, self.yVals):
+                f.write(f"{x} {y}\n")
