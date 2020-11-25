1fec6e593af54871be146688a77a9880c2b5f218
Kevin Aschauer <kevin.aschauer@hotmail.com>
2020-11-25 19:43:45 +0100

    First tests with whole array operations

--- work/Aufgabe5_delooping/delooping.py
+++ work/Aufgabe5_delooping/delooping.py
@@ -6,8 +6,40 @@ This should be a standalone file
 import numpy as np
 import matplotlib.pyplot as plt
 
-def pointstosection(p):
+def deloop2(p):
+    pplusone = np.roll(p, -1, axis=0)
+    pmove = p
+    pmoveplusone = np.roll(pmove, -1, axis=0)
+    print('p = {}'.format(p))
+    print(np.shape(p))
+    #print('pplusone = {}'.format(pplusone))
+    #print(np.shape(pplusone))
+    print('pmove = {}'.format(pmove))
+    print(np.shape(pmove))
+    #print('pmoveplusone = {}'.format(pmoveplusone))
+    #print(np.shape(pmoveplusone))
+    a_1 = pplusone - p
+    a_1 = np.reshape(a_1, 14, 'C')
+    a_2 = -pmoveplusone + pmove
+    a_2 = np.reshape(a_2, 14, 'C')
+    a = np.transpose(np.array([a_1, a_2]))
 
+    b = (np.array(pmove - p))
+    print('a = {}'.format(a))
+    print(np.shape(a))
+    print('a_1 = {}'.format(a_1))
+    print(np.reshape(a_1, 14, 'C'))
+    print(np.reshape(a_1, 14, 'F'))
+    print(np.reshape(a_1, 14, 'A'))
+    print(np.shape(a_1))
+    print('a_2 = {}'.format(a_2))
+    print(np.shape(a_2))
+    print('a = {}'.format(a))
+    print(np.shape(a))
+    print('b = {}'.format(b))
+    print(np.shape(b))
+    erg = np.linalg.solve(a, b)
+    print(erg)
     return None
 
 
@@ -17,12 +49,12 @@ def deloop(p):
 
 
     for i, workingpoint in enumerate(p):
-        if i < 9:
+        if i < 7:
             workingpointplusone = p[i + 1, :]
             for j, movingpoint in enumerate(p):
                 if i == j:
                     continue
-                if j < 9:
+                if j < 7:
                     movingpointplusone = p[j + 1]
                     #print('i:{} und j:{}',format((i, j)))
                     #print(workingpoint)
@@ -57,24 +89,20 @@ def deloop(p):
 
 
 """ In zweidimensionalen Numpy Arrays werden die Punkte gespeichert
-x0 x1 x2 x3 x4 x5 x6 x7 x8 x9
-y0 y1 y2 y3 y4 y5 y6 y7 y8 y9
+x0 y0
+x1 y1
+x2 y2
+x3 y3
 """
 
-x = np.array((0.0, 1.0, 2.0, 1.0, 2.3, 5.0, 6.0, 7.0, 8.0, 9.0))
-y = np.array((5.0, 5.5, 5.3, 4.0, 6.5, 5.0, 6.0, 7.0, 8.0, 9.0))
+x = np.array((0.0, 1.0, 2.0, 1.0, 2.3, 5.0, 6.0))
+y = np.array((5.0, 5.5, 5.3, 4.0, 6.5, 5.0, 6.0))
 
-p = np.transpose(np.concatenate((x, y)).reshape((2, 10)))
-print(p)
-print(np.transpose(p[:, 1] - p[:, 0]))
-print((p[0, 1]))
-print(p[:,0])
-testmatrix = np.array([p[1, :] - p[0, :], -p[2, :] + p[1, :]])
-print(type(testmatrix))
-print(testmatrix.shape)
-print(np.transpose(testmatrix))
+p = np.transpose(np.concatenate((x, y)).reshape((2, 7)))
+
 print('Starting deloop')
-deloop(p)
+deloop2(p)
+#deloop(p)
 print('Stopping deloop')
 print('..............')
 print(np.array([p[1, :] - p[0, :], -p[2, :] + p[1, :]]))
