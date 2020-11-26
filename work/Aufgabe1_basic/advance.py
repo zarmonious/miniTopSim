552cd33808d64f3dce8bc4dbc22e700d05a4bf44
haberger <d.haberger@hotmail.com>
2020-11-26 18:51:11 +0100

    Aufgabe1_basic: added docstrings

--- work/Aufgabe1_basic/advance.py
+++ work/Aufgabe1_basic/advance.py
@@ -1,8 +1,23 @@
+"""
+Implements the functions to calculate the surface movement
+
+function advance: calculates the movement of the surface
+function timestep: calculates the timestep for a given time  
+
+"""
+
+
 import numpy as np
 from surface import Surface
 
 
 def advance(surface: Surface, dtime):
+    """
+    calculates the movement of the surface fpr a timestep dtime
+
+    :param surface: surface that is being calculated
+    :param dtime: timstep of the calculation
+    """
     v = 1.
     nX, nY = surface.normal_vector()
 
@@ -11,4 +26,16 @@ def advance(surface: Surface, dtime):
 
 
 def timestep(dtime, time, end_time):
+    """
+    calculates the timestep for a given time
+
+    returns the timestep if the calculation isnt overstepping the endtime
+    if it would overstep it returns the resttime to calculte to the endtime
+
+    :param dtime: timestep
+    :param time: current time in simulation
+    :param end_time: endtime of the simulation
+
+    :returns: correct timestep to reach the calculation endtime
+    """
     return dtime if (time + dtime) <= end_time else (end_time - time)
--- work/Aufgabe1_basic/surface.py
+++ work/Aufgabe1_basic/surface.py
@@ -1,3 +1,11 @@
+"""
+Defines the class Surface
+
+includes function 
+"normal_vector" which calculates the normal vectors in every point of the surface
+"plot" which plots the surface
+"write" which writes the surface points into a .srf file
+"""
 import numpy as np
 import init_surface
 import matplotlib.pyplot as plt
@@ -6,19 +14,32 @@ import matplotlib.pyplot as plt
 class Surface:
 
     def __init__(self):
+        """
+        Initializes the x and y-Values with the init_surface module
+        """
         self.xVals = np.arange(-50., 51., 1.)
         self.yVals = init_surface.init_surface(self.xVals)
 
     def normal_vector(self):
+        """
+        calculates the normal vectors in every point of the surface
+
+        calculates the vector between the nearest neigbours of the point
+        and returns the normal vector of it
+
+        :returns: x Values, y Values of the normal vectors
+        """
         x = np.zeros_like(self.xVals)
         y = np.zeros_like(self.yVals)
 
+        #start and endpoint only have one neighbor 
         x[0] = self.xVals[1] - self.xVals[0]
         y[0] = self.yVals[1] - self.yVals[0]
-
         x[-1] = self.xVals[-1] - self.xVals[-2]
         y[-1] = self.yVals[-1] - self.yVals[-2]
 
+
+        #right neigbour - left neigbour
         y[1:-1] = np.array(self.yVals[2::] - self.yVals[:-2:])
         x[1:-1] = np.array(self.xVals[2::] - self.xVals[:-2:])
 
@@ -27,13 +48,26 @@ class Surface:
         return (y / magnitude), (-x / magnitude)
 
     def plot(self, time):
+        """
+        plots the surface
 
+        :param time: current simulation time
+        """
         plt.plot(self.xVals, self.yVals, "x-", label=f"t = {time}")
         plt.title('Surface')
         plt.xlabel('x[nm]')
         plt.ylabel('y[nm]')
 
     def write(self, time, filename):
+        """
+        writes the surface values into an srf file
+
+        format: surface: <time> <npoints> x-positions y-positions
+                x[0] y[0]
+        
+        :param time: current simulation time
+        :param filename: name of the file
+        """
         with open(filename, 'a') as f:
             f.write(f"surface: {time} {len(self.xVals)} x-positions y-positions\n")
             for x, y in zip(self.xVals, self.yVals):
