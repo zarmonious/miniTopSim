9af0a28ef4a2b1a9b03ca61a1833c1a7aaca3d2d
Kevin Aschauer <kevin.aschauer@hotmail.com>
2020-12-09 14:47:32 +0100

    Aufgabe5_delooping: optimising the delooping and adding timing measurement

--- mini_topsim/surface.py
+++ mini_topsim/surface.py
@@ -13,18 +13,6 @@ import matplotlib.pyplot as plt
 import parameters as par
 
 
-class Segment_pair:
-    def __init__(self, p11, p12, p21, p22):
-        self.x11 = p11[0]
-        self.y11 = p11[1]
-        self.x12 = p12[0]
-        self.y12 = p12[1]
-        self.x21 = p21[0]
-        self.y21 = p21[1]
-        self.x22 = p22[0]
-        self.y22 = p22[1]
-
-
 class Surface:
 
     def __init__(self):
@@ -90,119 +78,96 @@ class Surface:
                 f.write(f"{x} {y}\n")
 
     def _update_points(self):
+        """TODO: Dockstring"""
         self.points = np.concatenate((self.xvals.reshape((self.xvals.size, 1)),
                                       self.yvals.reshape((self.xvals.size, 1))),
                                      axis=1)
 
     def _intersections(self, result):
-
+        """TODO: Dockstring"""
         zeroes = np.zeros(result.shape)
         ones = np.ones(result.shape)
-        first_row = zeroes < result
-        #print(first_row)
-        second_row = result < ones
-        #print(second_row)
-        intersection = first_row == second_row
+        first_column = zeroes < result
+        #print(first_column)
+        second_column = result < ones
+        #print(second_column)
+        intersection = first_column == second_column
+        #print(intersection)
         result_bool = np.logical_and(intersection[:, 0], intersection[:, 1])
         return result_bool
 
     def _calculate_new_points(self, results, result_bool):
-        self.newpoints = np.zeros((self.further_information.shape))
+        """TODO: Dockstring"""
+        self.newpoints = np.zeros(self.further_information.shape)
         self.newpoints[:] = np.NaN
         for k, result in enumerate(results):
             if result_bool[k]:
-                i = int(self.further_information[k, 0])
-                j = int(self.further_information[k, 1])
-                #print('i= {}'.format(i))
-                #print('j = {}'.format(j))
-                #print('point = {}'.format(self.points[i]))
+                i = self.further_information[k, 0]
+                # print('i= {}'.format(i))
+                # print('point = {}'.format(self.points[i]))
                 self.newpoints[k] = self.points[i] + (self.points[i + 1] -
-                                                self.points[i]) * result[0]
-        #print('new points = {}'.format(self.newpoints))
-
+                                                      self.points[i]) * result[
+                                                                             0]
+        # print('new points = {}'.format(self.newpoints))
 
     def _setvals(self):
+        """TODO: Dockstring"""
         self.xvals = self.points[:, 0]
         self.yvals = self.points[:, 1]
 
-    def _create_matrix(self):
-
-        self.number_of_segments = (int(self.points.size / 2) - 1)
-        self.number_of_pairs = int(
-            self.number_of_segments ** 2 / 2 + self.number_of_segments / 2)
-        self.a = np.zeros((self.number_of_pairs, 2, 2))
-        self.b = np.zeros((self.number_of_pairs, 2, 1))
+    def _create_matrices(self):
+        """TODO: Dockstring"""
+        number_of_segments = (int(self.points.size / 2) - 1)
+        number_of_pairs = int(
+            (number_of_segments ** 2) / 2 + number_of_segments / 2)
+        self.a = np.zeros((number_of_pairs, 2, 2))
+        self.b = np.zeros((number_of_pairs, 2, 1))
         self.further_information = np.zeros(
-            ((int(self.points.size / 2) - 1) ** 2, 2))
-
+            ((int(self.points.size / 2) - 1) ** 2, 2), dtype=int)
         self.k = 0
+
         for i, pointi in enumerate(self.points):
-            if i < int(self.points.size / 2) - 1:
-                pointiplusone = self.points[i + 1, :]
+            if i < number_of_segments:
+                pointiplusone = self.points[i + 1]
                 for j, pointj in enumerate(self.points):
-                    if i + 1 < j and j < int(self.points.size / 2) - 1:
-                        #print('k = {}'.format(self.k))
-                        pointjplusone = self.points[j + 1, :]
-                        element = (
-                            pointiplusone - pointi, -pointjplusone + pointj)
-                        if np.linalg.det(np.transpose(element)) != 0:
-                            self.a[self.k] = np.transpose(element)
+                    if i + 1 < j and j < number_of_segments:
+                        # print('k = {}'.format(self.k))
+                        pointjplusone = self.points[j + 1]
+                        element = np.transpose((
+                            pointiplusone - pointi, -pointjplusone + pointj))
+                        if np.linalg.det(element) != 0:
+                            self.a[self.k] = element
                             self.b[self.k] = (pointj - pointi).reshape(2, 1)
                             self.further_information[self.k] = (i, j)
                             self.k = self.k + 1
 
-    def _compare_segments(self, segment_pair):
-        a = [[segment_pair.x12 - segment_pair.x11,
-              -segment_pair.x22 + segment_pair.x21],
-             [segment_pair.y12 - segment_pair.y11,
-              -segment_pair.y22 + segment_pair.y21]]
-
-        b = [[segment_pair.x21 - segment_pair.x11],
-             [segment_pair.y21 - segment_pair.y11]]
-        # print('Checking for intersection!')
-        # print('a is: {}'.format(a))
-        # print('Determinat is: {}'.format(np.linalg.det(a)))
-        if np.linalg.det(a) != 0:
-            # print('Determinat is non Zero')
-            erg = np.linalg.solve(a, b)
-            if 0 < erg[0] < 1 and 0 < erg[1] < 1:
-                # print('Intersection found!')
-                return np.transpose(erg)
-
-    def _intersectionpoint(self, result, i, j):
-        newpoint = self.points[i, :] + (
-                self.points[i + 1, :] - self.points[i, :]
-        ) * result[0, 0]
-        # print('new Point: {}'.format(newpoint))
-        return np.reshape(newpoint, (1, 2))
-
     def _flaglooppoints(self, result_bool):
-
+        """TODO: Dockstring"""
         for k, intersection in enumerate(result_bool):
             if intersection:
-                i = int(self.further_information[k, 0])
-                j = int(self.further_information[k, 1])
-                intervall = np.arange(i+1, j+1)
-                for element in intervall:
-                    self.points[element, :] = np.array((None, None))
+                i = self.further_information[k, 0]
+                j = self.further_information[k, 1]
+                interval = np.arange(i + 1, j + 1)
+                for point_index in interval:
+                    self.points[point_index] = np.array((None, None))
 
-    def _insertintersectionpoint(self, result_bool):
+    def _insertintersectionpoints(self, result_bool):
+        """TODO: Dockstring"""
         for k, newpoint in enumerate(self.newpoints):
             if not np.isnan(newpoint[0]):
-                i = int(self.further_information[k, 0])
-                j = int(self.further_information[k, 1])
-                #print('k = {}'.format(k))
-                #print('i= {}'.format(i))
-                #print('j = {}'.format(j))
-                #print('newpoint = {}'.format(newpoint))
-                self.points[i+1] = newpoint
+                i = self.further_information[k, 0]
+                # print('k = {}'.format(k))
+                # print('i= {}'.format(i))
+                # print('newpoint = {}'.format(newpoint))
+                self.points[i + 1] = newpoint
 
     def _removeflaged(self):
-        intervall = np.zeros((1, 0), dtype=int)
+        """TODO: Dockstring"""
+        interval = np.zeros((1, 0), dtype=int)
         for i, point in enumerate(self.points):
             if np.isnan(point[0]):
-                intervall = np.append(intervall, i)
-        self.points = np.delete(self.points, intervall, axis=0)
+                interval = np.append(interval, i)
+        self.points = np.delete(self.points, interval, axis=0)
 
     def deloop(self):
         """This method removes the loopes created by advancing the simulation
@@ -211,27 +176,12 @@ class Surface:
 
         :param <>:
         """
-        # print('Entering deloop!')
-        # print('in deloop yvals : {}'.format(self.yvals))
-        # print(self.xvals.reshape((self.xvals.size, 1)).shape)
-        # (self.points)
         self._update_points()
-        #print('Before deloop = {}'.format(self.points))
-        self._create_matrix()
-        # print('Compare Matrix shape: {}'.format(compare_matrix.shape))
-        # print(compare_matrix[0,0].x11)
-        # print(compare_matrix[0, 0].y11)
+        self._create_matrices()
         result = np.linalg.solve(self.a[:self.k], self.b[:self.k])
-        #print('reslult shape = {}'.format(result.shape))
         result_bool = self._intersections(result)
-        #print('The result_bool is: {}'.format(result_bool))
         self._calculate_new_points(result, result_bool)
         self._flaglooppoints(result_bool)
-
-        # print('points after inserting none outside the method: {}'.format(self.points))
-        self._insertintersectionpoint(result_bool)
-        #print('number of points = {}'.format(self.points.shape))
-        #print('number of xvals = {}'.format(self.xvals.shape))
+        self._insertintersectionpoints(result_bool)
         self._removeflaged()
         self._setvals()
-        #print('After deloop = {}'.format(self.points))
