c90eafd5d877c21f1359b5332cec78e78076d6f1
Helix2551 <thomas.helix@gmx.at>
2020-12-11 12:58:01 +0100

    Aufgabe15_AB: Abgabe files

---
+++ work/Aufgabe15_AB/Abgabe_Reisinger/init_surface.py
@@ -0,0 +1,256 @@
+"""
+Implements a function "init_surface" to initialize the Surface
+"""
+
+import numpy as np
+
+
+import parameters as par
+
+
+def init_surface(xvals):
+    """
+    initializes the starting values of the Surface 
+
+    INITIAL_SURFACE_TYPE can be: 
+        "Flat","Cosine","DoubleCosine","Step","V-Shape","File"
+    
+    In case of "File":
+        A srf-File from INTITIAL_SURFACE_FILE will be read and used as input.
+    
+    Parameters
+    ----------
+    xvals : np.array
+        x-values of the surface.
+
+    Returns
+    -------
+    yvals : np.array
+        y-Values of the initialized surface.
+    """
+    
+    if par.INITIAL_SURFACE_TYPE == 'Flat':
+        yvals=_flat(xvals)
+        
+    elif par.INITIAL_SURFACE_TYPE == 'DoubleCosine':
+        yvals=_double_cosine(xvals)
+        
+    elif par.INITIAL_SURFACE_TYPE == 'Step':
+        yvals=_step(xvals)
+        
+    elif par.INITIAL_SURFACE_TYPE == 'V-Shape':
+        yvals=_vshape(xvals)
+        
+        
+    else:
+        yvals=_cosine(xvals)
+        
+        
+    return yvals
+
+def _flat(xvals):
+    """
+    Function for flat surface y=0, with boundary conditions
+
+    Parameters
+    ----------
+    xvals : np.array
+        x-values of the surface.
+
+    Returns
+    -------
+    yvals : np.array
+        y-Values of the initialized surface.
+
+    """
+    yvals = np.zeros_like(xvals)
+    return yvals
+
+
+def _cosine(xvals):
+    """
+    Function for cosine surface with boundary conditions
+
+    Parameters
+    ----------
+    xvals : np.array
+        x-values of the surface.
+
+    Returns
+    -------
+    yvals : np.array
+        y-Values of the initialized surface.
+
+    """
+    mask = ((xvals >= par.FUN_XMIN) & (xvals <= par.FUN_XMAX))
+    yvals = np.zeros_like(xvals)
+    
+    yvals[mask] = ((par.FUN_PEAK_TO_PEAK / 2) * (1 +
+        np.cos((2*np.pi*xvals[mask])/50))) 
+    
+    return yvals
+
+def _double_cosine(xvals):
+    """
+    Function for double cosine surface with boundary conditions
+
+    Parameters
+    ----------
+    xvals : np.array
+        x-values of the surface.
+
+    Returns
+    -------
+    yvals : np.array
+        y-Values of the initialized surface.
+
+    """
+    mask = ((xvals >= par.FUN_XMIN) & (xvals <= par.FUN_XMAX))
+    yvals = np.zeros_like(xvals)
+    
+    yvals[mask] = ((par.FUN_PEAK_TO_PEAK / 2) * (1 +
+        np.cos(((4*np.pi*xvals[mask])+50*np.pi)/50))) 
+    
+    return yvals
+
+def _step(xvals):
+    """
+    Function for step surface with boundary conditions
+    
+    Step function with or without angle of inclination (depending on:
+    FUN_XMIN=FUN_XMAX or FUN_XMIN<FUN_XMAX).                                                   
+
+    Parameters
+    ----------
+    xvals : np.array
+        x-values of the surface.
+
+    Returns
+    -------
+    yvals : np.array
+        y-Values of the initialized surface.
+
+    """
+    mask = ((xvals >= par.FUN_XMIN) & (xvals <= par.FUN_XMAX))
+    mask_min = (xvals<par.FUN_XMIN)
+    
+    yvals = np.zeros_like(xvals)
+    yvals[mask_min] = par.FUN_PEAK_TO_PEAK
+    
+
+    k,d = _find_linar_poly(par.FUN_XMIN, par.FUN_PEAK_TO_PEAK, par.FUN_XMAX, 0)
+
+
+    yvals[mask] = k*xvals[mask] + d
+
+    return yvals
+
+def _vshape(xvals):
+    '''
+    Function for v-shape surface with boundary conditions
+
+    Parameters
+    ----------
+    xvals : np.array
+        x-values of the surface.
+
+    Returns
+    -------
+    yvals : np.array
+        y-Values of the initialized surface.
+
+    '''
+
+    center = par.FUN_XMIN + (par.FUN_XMAX-par.FUN_XMIN)/2
+
+    mask_left = ((xvals >= par.FUN_XMIN) & (xvals <= center))
+    mask_right = ((xvals >= center) & (xvals <= par.FUN_XMAX))
+
+    yvals = np.zeros_like(xvals)
+    
+
+    k1,d1 = _find_linar_poly(par.FUN_XMIN, 0, center, par.FUN_PEAK_TO_PEAK)
+    k2,d2 = _find_linar_poly(center, par.FUN_PEAK_TO_PEAK, par.FUN_XMAX, 0)
+
+
+    yvals[mask_left] = k1*xvals[mask_left] + d1
+    yvals[mask_right] = k2*xvals[mask_right] + d2
+    
+    return yvals
+
+
+
+#def _file()
+
+def _find_linar_poly(p1_x, p1_y, p2_x, p2_y):
+    """
+    Finding the linear polynomial y=kx+d from two points
+
+    Parameters
+    ----------
+    p1_x : float
+        X value of first point.
+    p1_y : float
+        Y value of first point.
+    p2_x : float
+        X value of second point.
+    p2_y : float
+        Y value of second point.
+
+    Returns
+    -------
+    k : float
+        Slope of the linear polynomial.
+    d : float
+        Intersection with y-axis.
+
+    """
+    k = (p2_y-p1_y)/(p2_x - p1_x)
+    d = p1_y - k * p1_x
+    return k,d
+
+def read_srf_file(filename,time):
+    '''
+    Reads x/y Values from srf-file at given time_stamp
+
+    Parameters
+    ----------
+    filename : str
+        Absolut path of given srf-file.
+    time : float
+        The time stamp where data will be taken.
+        
+    Returns
+    -------
+    xvalues : np.array
+        x-Values of the initialized surface.
+    yvalues : np.array
+        y-Values of the initialized surface.
+
+    '''
+    xvalues_list = list()
+    yvalues_list = list()
+
+    n_values = 0
+
+    
+    with open(filename) as file:
+
+        for line_index, line in enumerate(file):
+            if 'surface:' +' '+ str(time) in line:
+                string_array = line.split(' ')
+
+                n_values=(int(string_array[2]))
+                
+                for n, line in enumerate(file):
+                    if n<n_values:
+                        value_array = line.split(' ')
+                        xvalues_list.append(float(value_array[0]))
+                        yvalues_list.append(float(value_array[1]))
+                        
+    xvalues = np.array(xvalues_list)
+    yvalues = np.array(yvalues_list)
+               
+                  
+    return xvalues, yvalues
+
---
+++ work/Aufgabe15_AB/Abgabe_Reisinger/surface.py
@@ -0,0 +1,88 @@
+"""
+Defines the class Surface
+
+includes function 
+"normal_vector" calculates the normal vectors in every point of the surface
+"plot" which plots the surface
+"write" which writes the surface points into a .srf file
+"""
+import numpy as np
+import init_surface as init
+import matplotlib.pyplot as plt
+import os
+
+import parameters as par
+
+class Surface:
+
+    def __init__(self):
+        """
+        Initializes the x and y-Values with the init_surface module
+        """
+        if par.INITIAL_SURFACE_TYPE == 'File':
+            
+            srf_file = os.path.join(os.path.dirname(__file__), 
+                                    par.INITIAL_SURFACE_FILE)
+
+            
+            self.xvals, self.yvals = init.read_srf_file(srf_file, 
+                                                        par.TOTAL_TIME)
+
+        else:    
+            self.xvals = np.arange(par.XMIN, par.XMAX + 1, par.DELTA_X)
+            self.yvals = init.init_surface(self.xvals)
+
+    def normal_vector(self):
+        """
+        calculates the normal vectors in every point of the surface
+
+        calculates the vector between the nearest neigbours of the point
+        and returns the normal vector of it
+
+        :returns: x Values, y Values of the normal vectors
+        """
+        dx = np.zeros_like(self.xvals)
+        dy = np.zeros_like(self.yvals)
+
+        # start and endpoint only have one neighbor
+        dx[0] = self.xvals[1] - self.xvals[0]
+        dy[0] = self.yvals[1] - self.yvals[0]
+        dx[-1] = self.xvals[-1] - self.xvals[-2]
+        dy[-1] = self.yvals[-1] - self.yvals[-2]
+
+        # right neigbour - left neigbour
+        dy[1:-1] = self.yvals[2:] - self.yvals[:-2]
+        dx[1:-1] = self.xvals[2:] - self.xvals[:-2]
+
+        magnitude = np.linalg.norm([dx, dy], axis=0)
+
+        nx = dy / magnitude
+        ny = -dx / magnitude
+        return nx, ny
+
+    def plot(self, time):
+        """
+        plots the surface
+
+        :param time: current simulation time
+        """
+        plt.plot(self.xvals, self.yvals, "x-", label=f"t = {time}")
+        plt.title('Surface')
+        plt.xlabel('x[nm]')
+        plt.ylabel('y[nm]')
+
+    def write(self, time, filename):
+        """
+        writes the surface values into an srf file
+
+        format: surface: <time> <npoints> x-positions y-positions
+                x[0] y[0]
+
+        :param time: current simulation time
+        :param filename: name of the file
+        """
+        with open(filename, 'a') as f:
+            f.write(
+                f"surface: {time} {len(self.xvals)} x-positions y-positions\n")
+            for x, y in zip(self.xvals, self.yvals):
+                f.write(f"{x} {y}\n")
